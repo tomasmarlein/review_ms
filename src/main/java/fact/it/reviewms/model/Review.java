a3f024a692c733c69dcda09fcbe9589eaf4998bb
vanhouttejan4 <38554039+vanhouttejan4@users.noreply.github.com>
2020-12-01 14:28:45 +0100

    create review model

---
+++ src/main/java/fact/it/reviewms/model/Review.java
@@ -0,0 +1,52 @@
+package fact.it.reviewms.model;
+
+import javax.persistence.Id;
+
+public class Review {
+    @Id
+    private String id;
+    private Integer movieId;
+    private String text;
+    private double rating;
+
+    public Review() {
+    }
+
+    public Review(Integer movieId, String text, double rating) {
+        this.movieId = movieId;
+        this.text = text;
+        this.rating = rating;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public Integer getMovieId() {
+        return movieId;
+    }
+
+    public void setMovieId(Integer movieId) {
+        this.movieId = movieId;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public double getRating() {
+        return rating;
+    }
+
+    public void setRating(double rating) {
+        this.rating = rating;
+    }
+}
