d1ab7e6c5c4f93ec81c826bbe6a291ab03a647f1
vanhouttejan4 <38554039+vanhouttejan4@users.noreply.github.com>
2020-12-23 14:19:45 +0100

    added decimals to rank

--- src/test/java/fact/it/reviewms/ReviewmsControllerUnitTests.java
+++ src/test/java/fact/it/reviewms/ReviewmsControllerUnitTests.java
@@ -54,10 +54,10 @@ public class ReviewmsControllerUnitTests {
                 .andExpect(jsonPath("$[0].movieUuid", is(1)))
                 .andExpect(jsonPath("$[0].text", is("review 1 movie 1")))
                 .andExpect(jsonPath("$[0].rating", is(7.25)))
-                .andExpect(jsonPath("$[1].uuid", is(2)))
+                .andExpect(jsonPath("$[1].uuid", is(2.0)))
                 .andExpect(jsonPath("$[1].movieUuid", is(1)))
                 .andExpect(jsonPath("$[1].text", is("review 2 movie 1")))
-                .andExpect(jsonPath("$[1].rating", is(2)));
+                .andExpect(jsonPath("$[1].rating", is(2.0)));
     }
 
     @Test
@@ -84,7 +84,7 @@ public class ReviewmsControllerUnitTests {
                 .andExpect(jsonPath("$[1].uuid", is(3)))
                 .andExpect(jsonPath("$[1].movieUuid", is(3)))
                 .andExpect(jsonPath("$[1].text", is("best rated")))
-                .andExpect(jsonPath("$[1].rating", is(10)));
+                .andExpect(jsonPath("$[1].rating", is(10.0)));
     }
 
     @Test
@@ -106,10 +106,10 @@ public class ReviewmsControllerUnitTests {
                 .andExpect(jsonPath("$[0].movieUuid", is(1)))
                 .andExpect(jsonPath("$[0].text", is("review 1 movie 1")))
                 .andExpect(jsonPath("$[0].rating", is(7.25)))
-                .andExpect(jsonPath("$[1].uuid", is(2)))
+                .andExpect(jsonPath("$[1].uuid", is(2.0)))
                 .andExpect(jsonPath("$[1].movieUuid", is(1)))
                 .andExpect(jsonPath("$[1].text", is("review 2 movie 1")))
-                .andExpect(jsonPath("$[1].rating", is(2)));
+                .andExpect(jsonPath("$[1].rating", is(2.0)));
     }
 
     @Test
